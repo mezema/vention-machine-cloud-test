b4bc5999eb97aa77713deafc54c9352a8345a9cb
Mark Ezema <43732681+mezema@users.noreply.github.com>
2022-12-24 15:28:21 -0500

    stator init

--- apps/database/orm-config.ts
+++ apps/database/orm-config.ts
@@ -3,10 +3,10 @@ import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConne
 const ormConfig: PostgresConnectionOptions = {
   type: "postgres",
   host: process.env.DATABASE_HOST || "localhost",
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5433,
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5434,
   username: process.env.DATABASE_USERNAME || "postgres",
   password: process.env.DATABASE_PASSWORD || "postgres",
-  database: process.env.DATABASE_NAME || "stator",
+  database: process.env.DATABASE_NAME || "vention_machine_cloud_test",
   synchronize: !!(process.env.DATABASE_SYNCHRONIZE ?? true),
   entities: [__dirname + "/../../libs/**/*.entity{.ts,.js}"],
   migrations: [__dirname + "/migrations/*.ts"],
